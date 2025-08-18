788db3d5c952aad975dd9dfade7678424ce0f2f9
Bruno Pulheze <brunopulheze@hotmail.com>
2025-08-18 22:49:38 +0200

    MVP ready

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ from cart_app import router as cart_router
 
 app = FastAPI()
 
+origins = [
+    "https://ecommerce-backend-6zjk.onrender.com",  # your deployed frontend
+    "http://localhost:3000"              # local development (React default)
+]
+
 # Add CORS middleware to the main app
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
